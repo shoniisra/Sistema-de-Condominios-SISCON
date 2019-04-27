c46e671829cab84407f6ff80937c94a24dfd7cd3
Israel Teneda <israteneda@gmail.com>
2019-04-27 17:07:41 -0500

    add getter setter Proveedor Servicio

--- src/main/java/com/demo/siscon/models/entities/ProveedorServicio.java
+++ src/main/java/com/demo/siscon/models/entities/ProveedorServicio.java
@@ -44,4 +44,59 @@ public class ProveedorServicio implements Serializable {
         this.idProveedorServicio = id;
     }
 
+    public Integer getIdProveedorServicio() {
+        return idProveedorServicio;
+    }
+
+    public void setIdProveedorServicio(Integer idProveedorServicio) {
+        this.idProveedorServicio = idProveedorServicio;
+    }
+
+    public String getRUC() {
+        return RUC;
+    }
+
+    public void setRUC(String RUC) {
+        this.RUC = RUC;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public float getFrecuencia() {
+        return frecuencia;
+    }
+
+    public void setFrecuencia(float frecuencia) {
+        this.frecuencia = frecuencia;
+    }
 }
