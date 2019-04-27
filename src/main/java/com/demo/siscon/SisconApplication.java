41717252e086a25ff400df453d4fbebb7be4807b
Unknown <villajohnny@gmail.com>
2019-04-27 15:08:41 -0500

    creating proyect

---
+++ src/main/java/com/demo/siscon/SisconApplication.java
@@ -0,0 +1,13 @@
+package com.demo.siscon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisconApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SisconApplication.class, args);
+    }
+
+}
